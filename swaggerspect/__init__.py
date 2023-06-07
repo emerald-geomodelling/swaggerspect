0bd956b3c61b9f7b9f706b58e6e7d09cef94c325
Egil <egil.moller@freecode.no>
2023-06-07 17:59:55 +0200

    Proper swagger syntax for classes

--- swaggerspect/__init__.py
+++ swaggerspect/__init__.py
@@ -2,10 +2,44 @@ import inspect
 import numpydoc.docscrape
 import ast
 import typing
+import importlib.metadata
 
 def _get_name(obj):
     if obj is None: return None
     return obj.__module__ + "." + obj.__name__
+
+typemap = {
+    'builtins.str': 'string',
+    'builtins.int': 'integer',
+    'builtins.float': 'number',
+    'builtins.bool': 'boolean',
+    'builtins.list': 'array',
+    'builtins.tuple': 'array'
+}
+
+def make_type_schema(*typenames):
+    for typename in typenames:
+        if typename is inspect._empty:
+            continue
+        if typename is None:
+            continue
+        if not isinstance(typename, str):
+            if not isinstance(typename, type):
+                typename = type(typename)
+            typename = _get_name(typename)
+        return {"type": typemap.get(typename, "object"),
+                "x-python-type": typename}
+    return {}
+
+def make_value_schema(*values):
+    for value in values:
+        if value is inspect._empty:
+            continue
+        if value is None:
+            continue
+        if isinstance(value, (int, str, dict, list, bool)):
+            return {"default": value}
+    return {}
 
 def _merge_list(a, b):
     akeys = [p["name"] for p in a]
@@ -42,7 +76,7 @@ def _get_class_property_comments(cls):
     
     try:
         src = inspect.getsource(cls)
-    except TypeError:
+    except (TypeError, OSError):
         return propdocs
     
     body = ast.parse(src).body[0]
@@ -57,10 +91,10 @@ def _get_class_property_comments(cls):
     return propdocs
 
 def get_class_api_parameters_comments(cls):
-    return [{"name": n, "summary": v} for n, v in _get_class_property_comments(cls).items()]
+    return [{"name": n, "in": "query", "description": v} for n, v in _get_class_property_comments(cls).items()]
 
 def get_class_api_parameters_inspect(cls):
-    return [{"name": n, "schema": {} if v is None else {"default": v, "type": _get_name(type(v))}}
+    return [{"name": n, "in": "query", "schema": {} if v is None else merge(make_type_schema(v), make_value_schema(v))}
             for n, v in inspect.getmembers(cls)
             if (not n.startswith('__')
                 and not inspect.ismethod(v)
@@ -68,7 +102,7 @@ def get_class_api_parameters_inspect(cls):
                 and not inspect.isdatadescriptor(v))]
     
 def get_class_api_parameters_typing(cls):
-    return [{"name": k, "schema": {"type": _get_name(t)}} for k, t in typing.get_type_hints(cls).items()]
+    return [{"name": k, "in": "query", "schema": make_type_schema(t)} for k, t in typing.get_type_hints(cls).items()]
 
 def get_class_api(cls):
     args = merge(get_class_api_parameters_typing(cls),
@@ -77,28 +111,30 @@ def get_class_api(cls):
     return {
         "operationId": _get_name(cls),
         "description": inspect.getdoc(cls),
-        "parameters": args
-    }
+        "parameters": args,
+        "responses": {"default":
+                      {"description": _get_name(cls),
+                       "content": {
+                           "application/json": {"schema": make_type_schema(cls)}}}}}
 
 
 def get_function_api_parameters_inspect(fn):
     return [remove_empty({"name": k,
-                          "schema": {"default": None if v.default is inspect._empty else v.default,
-                                     "type": _get_name(
-                                         (None
-                                          if v.default is inspect._empty
-                                          else type(v.default))
-                                         if v.annotation is inspect._empty
-                                         else v.annotation)}})
+                          "in": "query",
+                          "schema": merge(
+                              make_type_schema(v.default, v.annotation),
+                              make_value_schema(v.default))})
              for k, v in inspect.signature(fn).parameters.items()]
 
 def get_function_api_parameters_comments(fn):
     docs = inspect.getdoc(fn)
+    if docs is None: return []
     docast = numpydoc.docscrape.NumpyDocString(docs)
     
     return [{"name": p.name,
+             "in": "query",
              "description": "\n".join(p.desc),
-             "schema": {"type": p.type}}
+             "schema": make_type_schema(p.type)}
             for p in docast["Parameters"]]
     
 def get_function_api(fn):
@@ -106,12 +142,68 @@ def get_function_api(fn):
                  get_function_api_parameters_comments(fn)) 
 
     docs = inspect.getdoc(fn)
-    docast = numpydoc.docscrape.NumpyDocString(docs)
-    description = "\n".join(docast["Summary"] + docast["Extended Summary"])
-    
+    description = _get_name(fn)
+    if docs is not None:
+        docast = numpydoc.docscrape.NumpyDocString(docs)
+        description = "\n".join(docast["Summary"] + docast["Extended Summary"])
+
+    returntype = inspect.signature(fn).return_annotation
+        
     return {
         "operationId": _get_name(fn),
         "description": description,
-        "parameters": args
-    }
+        "parameters": args,
+        "responses": {"default":
+                      {"description": _get_name(fn),
+                       "content": {
+                           "application/json": remove_empty({
+                               "schema": make_type_schema(returntype)})}}}}
 
+def get_api(obj):
+    if type(obj) is type:
+        return get_class_api(obj)
+    elif inspect.isfunction(obj):
+        return get_function_api(obj)
+    return None
+
+def get_apis_dict(objs):
+    return {
+        "openapi": '3.0.3',
+        "info": {"title": "unknown",
+                 "version": "1.0",
+                 "description": "unknown"},
+        "paths": {
+            "/" + k: {"get": v}
+            for k, v in
+            [(k, get_api(v))
+             for k, v in objs.items()]
+            if v
+        }}
+
+def get_apis_entrypoints(group):
+    entrypoints = importlib.metadata.entry_points()
+    if group not in entrypoints: return {}
+    entrypoints = entrypoints[group]
+    docs = get_apis_dict({entry.name: entry.load()
+                          for entry in entrypoints})
+    docs["info"] = {"title": group,
+                    "version": "1.0",
+                    "description": group}
+    return docs
+    
+def get_apis_module(module):
+    if isinstance(module, str):
+        try:
+            module = importlib.import_module(module)
+        except:
+            return {}
+    docs = get_apis_dict({name: getattr(module, name)
+                         for name in dir(module)
+                         if not name.startswith("__")})
+    docs["info"] = {"title": module.__name__,
+                    "version": "1.0",
+                    "description": inspect.getdoc(module) or module.__name__}
+    return docs
+
+def get_apis(objs):
+    return merge(get_apis_module(objs), get_apis_entrypoints(objs))

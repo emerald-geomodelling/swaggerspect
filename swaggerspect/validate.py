3bb554291ad9c1c8c6d799b9bd956e697a3ba7f6
Egil <egil.moller@freecode.no>
2023-11-09 11:16:53 +0100

    Bugfix for schema key ordering

--- swaggerspect/validate.py
+++ swaggerspect/validate.py
@@ -1,5 +1,6 @@
 """Validation tools"""
 
+
 import jsonschema
 
 __all__ = ["GroupMergingValidator"]
@@ -12,13 +13,24 @@ def _merge_groups(validator, properties, instance, schema):
 
     for property, subschema in properties.items():
         if subschema.get("x-python-type", True) is None:
-            for key, value in instance.pop(property).items():
-                instance[property + "__" + key] = value
+            validator.renames.append((instance, property))
 
-GroupMergingValidator = jsonschema.validators.extend(
-    jsonschema.Draft202012Validator, {"properties" : _merge_groups})
 
-GroupMergingValidator.__doc__ = """jsonchema subclass that validates
+class GroupMergingValidator(jsonschema.validators.extend(
+    jsonschema.Draft202012Validator, {"properties" : _merge_groups})):
+    """jsonchema subclass that validates
 data against a schema, but also transforms your data ungrouping any
 parameters grouped by `get_apis(objs, group_parameters = True)`.
 """
+    
+    def evolve(self, **changes):
+        res = super(GroupMergingValidator, self).evolve(**changes)
+        res.renames = self.renames
+        return res
+        
+    def validate(self, data):
+        self.renames = []
+        super(GroupMergingValidator, self).validate(data)
+        for instance, prop in self.renames:
+            for key, value in instance.pop(prop).items():
+                instance[prop + "__" + key] = value

6e1d70bb24d5805a95b0993e90d92405bea0da0e
Egil <egil.moller@freecode.no>
2023-11-09 14:05:33 +0100

    Workaround for new limitation in jsonschema

--- swaggerspect/validate.py
+++ swaggerspect/validate.py
@@ -7,30 +7,32 @@ __all__ = ["GroupMergingValidator"]
 
 validate_properties = jsonschema.Draft202012Validator.VALIDATORS["properties"]
 
-def _merge_groups(validator, properties, instance, schema):
-    for error in validate_properties(validator, properties, instance, schema):
-        yield error
 
-    for property, subschema in properties.items():
-        if subschema.get("x-python-type", True) is None:
-            validator.renames.append((instance, property))
-
-
-class GroupMergingValidator(jsonschema.validators.extend(
-    jsonschema.Draft202012Validator, {"properties" : _merge_groups})):
-    """jsonchema subclass that validates
+class GroupMergingValidator(object):
+    """jsonchema validator class that validates
 data against a schema, but also transforms your data ungrouping any
 parameters grouped by `get_apis(objs, group_parameters = True)`.
 """
-    
-    def evolve(self, **changes):
-        res = super(GroupMergingValidator, self).evolve(**changes)
-        res.renames = self.renames
-        return res
-        
+    def __init__(self, *arg, **kw):
+        self.renames = []
+        self.cls = jsonschema.validators.extend(
+            jsonschema.Draft202012Validator, {"properties" : self._merge_groups}) 
+        self.inst = self.cls(*arg, **kw)
+
+    def _merge_groups(self, validator, properties, instance, schema):   
+        for error in validate_properties(validator, properties, instance, schema):
+            yield error
+
+        for property, subschema in properties.items():
+            if subschema.get("x-python-type", True) is None:
+                self.renames.append((instance, property))
+
+    def __getattr__(self, name):
+        return getattr(self.inst, name)
+
     def validate(self, data):
         self.renames = []
-        super(GroupMergingValidator, self).validate(data)
-        for instance, prop in self.renames:
+        self.inst.validate(data)       
+        for instance, prop in self.renames:                                
             for key, value in instance.pop(prop).items():
                 instance[prop + "__" + key] = value
